6010bda0adb1843fa9fe34a7b5f54c45611ff4d1
yujin <sonewdim@gmail.com>
2022-12-12 16:02:37 +0900

    add todo list detail page

---
+++ src/components/Progressbar.js
@@ -0,0 +1,98 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 100%;
+  padding: 20px 0;
+`;
+
+const ContentWrapper = styled.div`
+  width: 97%;
+  padding-left: 3%;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  align-items: center;
+`;
+
+const ContentTitle = styled.h2`
+  font-weight: 800;
+  padding-bottom: 20px;
+`;
+
+const ProgressBarWrapper = styled.div`
+  position: relative;
+  width: 80%;
+  height: 30px;
+  border: 1px solid black;
+`;
+
+const ProgressBar = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: ${(props) => props.width};
+  height: 30px;
+  background-color: tomato;
+  transition: width 1s;
+`;
+
+const ProgressBarIcon = styled.span`
+  position: absolute;
+  right: -15px;
+  top: -15px;
+  font-size: 40px;
+  transform: rotateY(180deg);
+`;
+
+const StateWrapper = styled.div`
+  width: 18%;
+  font-size: 20px;
+  font-weight: 800;
+  padding-left: 2%;
+  display: flex;
+  align-items: center;
+  gap: 20px;
+`;
+
+const StateIcon = styled.span`
+  font-size: ${(props) => props.fontSize};
+  transition: font-size 1s;
+`;
+
+function Progressbar() {
+  const todos = useSelector((store) => store.todos.todos);
+  const donesCount = todos.filter((v) => v.isDone === true).length;
+  const donesPercentage = (donesCount / todos.length) * 100;
+  return (
+    <Wrapper>
+      <ContentTitle>progress bar</ContentTitle>
+      <ContentWrapper>
+        <ProgressBarWrapper>
+          <ProgressBar width={`${donesPercentage}%`}>
+            <ProgressBarIcon>🏃</ProgressBarIcon>
+          </ProgressBar>
+        </ProgressBarWrapper>
+        <StateWrapper>
+          <StateIcon
+            fontSize={
+              donesPercentage > 50
+                ? donesPercentage === 100
+                  ? "40px"
+                  : "20px"
+                : "16px"
+            }
+          >
+            🔥
+          </StateIcon>
+          <span>
+            {donesCount}/{todos.length}
+          </span>
+        </StateWrapper>
+      </ContentWrapper>
+    </Wrapper>
+  );
+}
+
+export default Progressbar;
--- src/redux/modules/TodosSlice.js
+++ src/redux/modules/TodosSlice.js
@@ -1,14 +1,91 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
   todos: [],
+  todosID: 0,
 };
+
+export const __getTodos = createAsyncThunk(
+  "getTodos",
+  async (payload, thunkAPI) => {
+    try {
+      const { data } = await axios.get(
+        `http://localhost:3001/todos/${payload}`
+      );
+      thunkAPI.dispatch(getTodos(data));
+    } catch (e) {
+      alert(`getTodoError: ${e}`);
+    }
+  }
+);
+
+export const __addTodo = createAsyncThunk(
+  "addTodo",
+  async (payload, thunkAPI) => {
+    try {
+      await axios.patch(`http://localhost:3001/todos/${payload.todosID}`, {
+        items: [...payload.todos, payload.todo],
+      });
+      thunkAPI.dispatch(addTodo(payload.todo));
+    } catch (e) {
+      alert(`addTodoError: ${e}`);
+    }
+  }
+);
+
+export const __updateTodo = createAsyncThunk(
+  "updateTodo",
+  async (payload, thunkAPI) => {
+    try {
+      const updatedTodos = payload.todos.map((v) => {
+        if (v.id === payload.todo.id) return payload.todo;
+        return v;
+      });
+      await axios.patch(`http://localhost:3001/todos/${payload.todosID}`, {
+        items: updatedTodos,
+      });
+      thunkAPI.dispatch(updateTodo(updatedTodos));
+    } catch (e) {
+      alert(`updateTodoError: ${e}`);
+    }
+  }
+);
+
+export const __deleteTodo = createAsyncThunk(
+  "deleteTodo",
+  async (payload, thunkAPI) => {
+    try {
+      const updatedTodos = payload.todos.filter((v) => v.id !== payload.todoID);
+      await axios.patch(`http://localhost:3001/todos/${payload.todosID}`, {
+        items: updatedTodos,
+      });
+      thunkAPI.dispatch(updateTodo(updatedTodos));
+    } catch (e) {
+      alert(`deleteTodoError: ${e}`);
+    }
+  }
+);
 
 const todosSlice = createSlice({
   name: "todos",
   initialState,
-  reducers: {},
+  reducers: {
+    getTodos: (state, action) => {
+      state.todosID = action.payload.id;
+      state.todos = action.payload.items;
+    },
+    addTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+    updateTodo: (state, action) => {
+      state.todos = action.payload;
+    },
+    deleteTodo: (state, action) => {
+      state.todos = action.payload;
+    },
+  },
 });
 
-export const { addNumber, minusNumber } = todosSlice.actions;
+export const { getTodos, addTodo, updateTodo, deleteTodo } = todosSlice.actions;
 export default todosSlice.reducer;
